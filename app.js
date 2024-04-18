0e0afa603c83b90dc0679e6cc877e1328677161c
Sihan <29PathiS@mbbc.qld.edu.au>
2024-04-18 22:19:00 +1000

    Added password restricted content

---
+++ app.js
@@ -0,0 +1,10 @@
+const passwordBox = document.getElementById('passwordBox');
+const secretText = document.getElementById('secretText');
+
+function checkPassword() {
+    if (passwordBox.value = "sihan") {
+        secretText.innerHTML = "Hello from Sihan!";
+    } else {
+        secretText.innerHTML = "Wrong Password!"
+    }
+}
