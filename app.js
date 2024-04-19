fb6f9dd6d0e1807d5aecf5141c2d11ad7c336b71
Sihan <29PathiS@mbbc.qld.edu.au>
2024-04-19 17:55:44 +1000

    Updated Code

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const secretText = document.getElementById('secretText');
 
 function checkPassword() {
     if (passwordBox.value == "sihan") {
-        secretText.innerHTML = "Hello from Sihan!";
+        secretText.innerHTML = "Follow me on instagram now! https://www.instagram.com/sihanpathiraja/ <:)";
     } else {
         secretText.innerHTML = "Wrong Password!";
         passwordBox.value == "";
